debf85781c0d68e28feba3a45a084e34c8e450e5
Minko Urchev <urchevminko@gmail.com>
2024-09-17 09:16:51 +0300

    set express-handelbars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const PORT = 5000;
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs')
+app.set('views', 'src/views')
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('home/index')
 })
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}...`));
